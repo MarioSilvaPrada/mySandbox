646259370e03076d83b91022c732806152171c25
MarioSilvaPrada <mariosilvaprada@gmail.com>
2018-10-27 15:33:04 +0100

    updating PP app

--- Week_3/app_homepage/script.js
+++ Week_3/app_homepage/script.js
@@ -1,15 +1,26 @@
 let hamburger = document.querySelector(".fa-bars");
 let modal = document.querySelector(".navbar-modal");
 
-let close = document.querySelector(".fa-times");
+// let close = document.querySelector(".fa-times");
 
 
+// set modal to width 0px
+modal.style.width = '0'
 //create toggle modal
 hamburger.addEventListener('click', () => {
-    modal.style.width = "100%";
+    console.log(modal.style.width)
+
+    if (modal.style.width == '0px') {
+        modal.style.width = "100%";
+        hamburger.classList = "fas fa-times";
+    } else {
+        modal.style.width = '0px';
+        hamburger.className = "fas fa-bars";
+    }
+
 });
 
-close.addEventListener('click', () => {
-    modal.style.width = "0%";
-})
+// close.addEventListener('click', () => {
+//     modal.style.width = "0%";
+// })
 
