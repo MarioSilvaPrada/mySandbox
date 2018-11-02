46aef91f03c0d8f9b64fec17cd46b38d8dcd48e9
MarioSilvaPrada <mariosilvaprada@gmail.com>
2018-11-02 17:50:09 +0000

    updating quote_maker

--- Week_5/quote_maker/script.js
+++ Week_5/quote_maker/script.js
@@ -2,11 +2,19 @@ let userName = prompt('What is your name?');
 let answer = prompt('Do you want to leave a quote for humanity to reflect upon such wisdom?');
 
 if (answer.toLowerCase() === 'yes') {
-    let number = prompt('How many words?')
+    let number = prompt('How many words?');
     alert('Tell me your quote word by word'); 
-    let quote = prompt('Tell me your quote word by word');
 
-    alert(`Your quote is: ${quote} - ${userName}`)
+    let wordArr = [];
+    
+    
+    for (let i = 0; i < number; i++) {
+        let word = prompt('give word');
+        wordArr.push(word);
+    }
+    let quote = wordArr.join(' ');
+
+    alert(`Your quote is: '${quote}.' - ${userName}`)
 
 } else if (answer.toLowerCase() === 'no') {
 
