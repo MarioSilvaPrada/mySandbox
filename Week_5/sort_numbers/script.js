f73f3fa9d9e2eed74403cd4511ae21a6cf0dad16
MarioSilvaPrada <mariosilvaprada@gmail.com>
2018-11-03 11:51:44 +0000

    updating sort_numbers

--- Week_5/sort_numbers/script.js
+++ Week_5/sort_numbers/script.js
@@ -1,1 +1,25 @@
 
+let numbArr = [];
+for (let i = 0; i < 3; i++) {
+    let numbers = parseFloat(prompt('input numbers'));
+    if(numbArr.length == 0 ) {
+        numbArr.push(numbers);
+    } else if (numbArr.length == 1 && numbers < numbArr[0]) {
+        numbArr.push(numbers);
+    } else if (numbArr.length == 1 && numbers > numbArr[0]) {
+        numbArr.unshift(numbers);
+    } else if (numbArr.length == 2  &&  numbers < numbArr[0] && numbers < numbArr[1]) {
+        numbArr.push(numbers);
+    } else if (numbArr.length == 2   && numbers < numbArr[0] && numbers > numbArr[1]) {
+        numbArr.splice(1,0,numbers);
+    } else if (numbArr.length == 2  && numbers > numbArr[0] && numbers > numbArr[1]) {
+        numbArr.unshift(numbers);
+    } else if (numbers == numbArr[0] || numbers == numbArr[1]) {
+        numbArr.push(numbers);
+    }
+}
+
+alert (`The order is ${numbArr.join(', ')}`);
+
+                                                                                                                                               
+
