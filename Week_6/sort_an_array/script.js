356c1ce5702c3de70c1852bbfc8938e02245efbb
MarioSilvaPrada <mariosilvaprada@gmail.com>
2018-11-05 23:46:50 +0000

    updating sort an array

---
+++ Week_6/sort_an_array/script.js
@@ -0,0 +1,47 @@
+
+let personName = prompt('user input');
+
+let mainArr = [];
+
+    while (personName !== 'terminate*') {
+        let personAge = parseFloat(prompt('age'));
+        let personProfession = prompt('profession');
+        mainArr.push([personName,personAge,personProfession]);
+        personName = prompt('user input');
+    }
+
+    if (mainArr.length > 0) {
+
+        let mainArrSorted = mainArr.slice();
+        for(let i=0; i < mainArrSorted.length-1 ;i++){
+            for(let j = i+1; j < mainArrSorted.length; j++){
+                if(mainArrSorted[i][1]>mainArrSorted[j][1]){
+                    let temp = mainArrSorted[i];
+                    mainArrSorted[i] = mainArrSorted[j];
+                    mainArrSorted[j] = temp;
+                }
+            }
+        }
+        
+        let output = [];
+    
+            for (let i = 0; i < mainArrSorted.length; i++) {
+                output.push(` ${mainArrSorted[i][0]} - ${mainArrSorted[i][1]}`);
+            }
+            
+            alert(`list:${output}`);
+            alert('Goodbye');
+
+    } else {
+        alert('You did not insert any person in the list');
+    }
+    
+    
+
+  
+
+
+
+
+
+    
