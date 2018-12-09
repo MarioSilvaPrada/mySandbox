49f91704d073e514e51b8c63172f35e5c07d1ce1
MarioSilvaPrada <mariosilvaprada@gmail.com>
2018-12-09 23:01:41 +0000

    updating

---
+++ Week_10/binary_search/02_big_O.js
@@ -0,0 +1,13 @@
+// To analyze the performance of an algorithm, we use Big O Notation.
+
+// Big O can give us a high level understanding of the Time or Space complexity of an algortithm. 
+// It doesn't care about precision, only about general trends (if is linear, quadratic, constant etc.)
+
+// Big O is a mathematical notation that describes the behavior 
+// of an alghorithm when inputs tends towards a particular value or infinity.
+
+// It allows us to talk formally about how the runtime of an
+// algortithm grow as the inputs grow.
+
+// The Time or Space complexity depends only on the algortihm, not the hardware used to run
+// the algorothm.
