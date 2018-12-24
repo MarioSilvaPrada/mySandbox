0e2aa7266fe0d5e96540873a27d2698344f826c3
MarioSilvaPrada <mariosilvaprada@gmail.com>
2018-12-24 09:12:22 +0000

    updating

--- Week_12/instagram_spa/script.js
+++ Week_12/instagram_spa/script.js
@@ -1,4 +1,4 @@
-import { userSign, userExist, userCreated } from './module.js';
+import * as htmlComponents from './module.js';
 
 
 
@@ -35,59 +35,16 @@ function getUser(mail) {
 
 const input = $('.input');
 
-input.html(userSign());
 
-let signBtn = $('.sign-btn');
+const addHtml = () => {
+    input.html(htmlComponents.userSign);
 
-signBtn.on('click', function () {
-    
-    let sign = $('.sign-status')[0];
-    if (sign.textContent === 'Log In') {
-        sign.textContent = 'Sign Up';
-        $('.sign-btn')[0].textContent = 'Log In';
-        $('h3').after(`<label for="" class='user-name'>Name</label>
-        <input type="text" placeholder="Your Name" class="u-full-width input-name">`)
+    let signBtn = $('.sign-btn');
+}
 
-    }
-    else {
-        sign.textContent = 'Log In';
-        $('.sign-btn')[0].textContent = 'Sign Up';
-        $('.user-name').hide();
-        $('.input-name').hide();
-    }
-});
+addHtml();
 
-$('input[type="submit"]').on('click', function () {
 
-    if (!isLogin) {
-        let name = $('.input-name').val();
-        let email = $('.input-email').val();
-        let password = $('.input-password').val();
-
-        // hasUser(email) ? input.html(userExist()) : createUser(name, email, password); console.log(userDatabase)
-
-        if (hasUser(email)) {
-
-           input.html(userExist())
-        }
-        else {
-            createUser(name, email, password); 
-            console.log(userDatabase)
-        }
-        
-        $('.input-name').val('');
-        $('.input-email').val('');
-        $('.input-password').val('');
-
-    }
-    else {
-        alert('log out first before you create a new account')
-    }
-    $('.ok-btn').on('click', function() {
-            input.html(userSign());
-        });
-
-})
 
 
 // // main file:
