98244f5366d1968f640baece35ac5b9d2bc499c6
MarioSilvaPrada <mariosilvaprada@gmail.com>
2018-12-24 16:57:34 +0000

    updating instagram SPA

--- Week_12/instagram_spa/module.js
+++ Week_12/instagram_spa/module.js
@@ -1,12 +1,19 @@
 export function userSign() {
- return `
+    return `
     <div class="signin">
         <div class = 'sign-msg'>
             <h3 class='sign-status'>Log In</h3>
+            
             <label for="">Email</label>
             <input type="email" placeholder="Your Email" class="u-full-width input-email">
             <label for="">Password</label>
             <input type="password" placeholder="Password" class="u-full-width input-password">
+            <div class = 'name-display'>
+            <label for="">Nome</label>
+            <input type="text" placeholder="Your Name" class="u-full-width input-name">
+            <label for="">Id / Username</label>
+            <input type="text" placeholder="Your ID" class="u-full-width input-id">
+            </div>
             <input type="submit" class='button-primary'> <br>
             <button class='sign-btn'>Sign Up</button>
         </div>
@@ -14,24 +21,50 @@ export function userSign() {
         `
 }
 
-export function userExist() {
+export function userMsg(msg) {
     return `
     <div class="user">
         <div class ='msg-user'>
-            <h5>Sorry, that email is already taken</h5>
+            <h5>${msg}</h5>
             <button class='button-primary ok-btn'>OK</button>
         </div>   
     </div>
         `
 }
 
-export function userCreated() {
-    return `
-    <div class="user">
-        <div class ='msg-user'>
-            <h5>Thank you for your registration, welcome!</h5>
-            <button class='button-primary ok-btn'>OK</button>
-        </div>   
+export function userProfile(user) {
+    return `<div class="main-page">
+    <div class="header">
+      <span>Welcome ${user.name}</span>
+      <div class="user-btns">
+        <button class='profile-btn'>My Profile</button>
+        <button class='logout-btn'>Log Out</button>
+      </div>
     </div>
-        `
+    <div class="search-field">
+        <input type="email" class='email-search' placeholder="Find Email">
+        <div class="btns-search">
+        <button class='search-btn'>Search</button>
+        <button class='follow-btn'>Follow this Person</button>
+        </div>
+    </div>
+  
+  
+
+    <div class="publish-field">
+        <h5>Publish</h5>
+        <label for="">Photo URL</label>
+        <input type="text">
+        <label for="">Description</label>
+        <textarea name="" id="" cols="30" rows="10"></textarea><br>
+        <button class= 'button-primary'>Publish</button>
+    </div>
+  
+    <div class="follow">
+        <span>Name:${user.name}</span>
+        <span>Followes: <strong>${user.followers}</strong></span>
+        <span>Following:<strong>${user.following}</strong> </span>
+    </div>
+  
+    </div>`
 }
--- Week_12/instagram_spa/script.js
+++ Week_12/instagram_spa/script.js
@@ -1,14 +1,13 @@
 import * as htmlComponents from './module.js';
 
-
-
 let userDatabase = [];
 let isLogin = false;
 let mailLoggedIn = '';
 
 class User {
-    constructor(name, email, password) {
+    constructor(name, email, password, id) {
         this.name = name;
+        this.id = id;
         this.email = email;
         this.password = password;
         this.followers = 0;
@@ -23,8 +22,8 @@ function hasUser(mailUser) {
     }) !== -1;
 }
 
-function createUser(name, email, password) {
-    userDatabase.push(new User(name, email, password));
+function createUser(name, email, password,id) {
+    userDatabase.push(new User(name, email, password,id));
 }
 
 function getUser(mail) {
@@ -33,16 +32,114 @@ function getUser(mail) {
     });
 }
 
+function isEmailValid(email) {
+    let reg = /^([a-zA-Z0-9\_\-\.]+)@([a-zA-Z0-9\_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+    return reg.test(email);
+}
+
 const input = $('.input');
 
 
-const addHtml = () => {
+const signUser = () => {
     input.html(htmlComponents.userSign);
 
     let signBtn = $('.sign-btn');
+
+    signBtn.on('click', function () {
+        if ($('.sign-status').text() === 'Log In') {
+            $('.sign-btn').text('Log In');
+            $('.sign-status').text('Sign Up');
+            $('.name-display').show();
+        }
+        else {
+            $('.sign-btn').text('Sign Up');
+            $('.sign-status').text('Log In');
+            $('.name-display').hide();
+        }
+    });
+
+    $('input[type="submit"]').on('click', function () {
+        if ($('.sign-status').text() === 'Sign Up') {
+            if (!isLogin) {
+                let name = $('.input-name').val();
+                let email = $('.input-email').val();
+                let password = $('.input-password').val();
+                let id = $('.input-id').val();
+
+                if (!isEmailValid(email)) {
+                    alert('invalid email');
+                }
+                else if (hasUser(email)) {
+                    input.html(htmlComponents.userMsg('Sorry, that email is already taken'));
+
+                    $('.ok-btn').on('click', function () {
+                        signUser();
+                    })
+                }
+                else {
+                    input.html(htmlComponents.userMsg('Thank you for your registration, welcome!'));
+                    createUser(name, email, password, id);
+                    console.log(userDatabase);
+
+                    $('.ok-btn').on('click', function () {
+                        signUser();
+                    })
+                }
+            }
+        }
+        else {
+            let email = $('.input-email').val();
+            let password = $('.input-password').val();
+
+            if (!isLogin) {
+                if (hasUser(email)) {
+                    let user = getUser(email);
+                    if (user.password === password) {
+                        mailLoggedIn = user.email;
+                        isLogin = true;
+
+                        input.html(htmlComponents.userMsg(`Welcome, ${user.name}`));
+
+                        $('.ok-btn').on('click', function () {
+                            page(user);
+                        })
+
+                        console.log(mailLoggedIn);
+                        console.log(isLogin);
+                    }
+                    else {
+                        alert('Your email or password is incorrect');
+                        $('.input-email').val('');
+                        $('.input-password').val('');
+                    }
+                    
+                }
+                else {
+                    input.html(htmlComponents.userMsg('We don\'t have that account'));
+
+                    $('.ok-btn').on('click', function () {
+                        signUser();
+                    })
+                }
+            }
+        }
+
+    })
+
 }
 
-addHtml();
+signUser();
+
+
+const page = (user) => {
+    input.html(htmlComponents.userProfile(user))
+}
+
+
+
+
+
 
 
 
