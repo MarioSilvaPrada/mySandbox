6cd8b41631cd02afac18a642bd8c0c09341c63d8
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-01-04 21:42:56 +0000

    updating

--- l. Week_13/04.async_code2/main.js
+++ l. Week_13/04.async_code2/main.js
@@ -4,12 +4,11 @@ fs.writeFile('input.txt', 'Hello my dear friend', (err) => {
     if (err) {
         return console.log(err);
     }
-    console.log('File was created!')
+    fs.readFile('input.txt', function (err, data) {
+        if (err) {
+           return console.error(err);
+        }
+        console.log("File created with the following text: " + data.toString());
+     });
 })
 
-fs.readFile('input.txt', function (err, data) {
-    if (err) {
-       return console.error(err);
-    }
-    console.log("File created with the following text: " + data.toString());
- });
