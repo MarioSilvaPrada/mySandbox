332c5e91c920d23743deb890a91a0477909f7264
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-01-16 18:46:23 +0000

    updating Week 14

---
+++ m. Week_14/03.video_ stream/app.js
@@ -0,0 +1,14 @@
+let http = require('http');
+let fs = require('fs');
+
+let server = http.createServer( function(req,res) {
+    console.log(`The request was made: ${req.url}`)
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    myReadStream.pipe(res);
+});
+
+server.listen(3000, '127.0.0.1');
+
+console.log('Running!')
