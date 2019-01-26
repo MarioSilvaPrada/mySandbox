02b115897f3c6160a41a288f4da79f0aeaddc22f
MarioSilvaPrada <mariosilvaprada@gmail.com>
2019-01-26 16:03:00 +0000

    updating

---
+++ n. Week_15/01. geolocation/app.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let request = require('request');
+
+let app = express();
+
+app.use(express.static('assets'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/geolocation', (req, res) => {
+    let location = req.query['location']
+    let url = `https://nominatim.openstreetmap.org/search?q=${location}&format=json`;
+   
+    const options = {
+        url: url,
+        headers: {
+            'User-Agent': 'request'
+        }
+    }
+
+    console.log(url);
+    request(options, function (error, response, body) {
+        res.send(response.body)
+    });
+})
+
+
+
+app.listen(3000, function () {
+    console.log("server started on localhost 3000");
+});
